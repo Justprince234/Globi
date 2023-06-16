972d669bf3d5477181c40674d240c1bd2a375799
Justprince234 <oilsunny9@gmail.com>
2023-06-16 21:39:08 +0000

    first commit

--- core/management/commands/createsu.py
+++ core/management/commands/createsu.py
@@ -1,14 +1,15 @@
 from account.models import User
 from django.core.management.base import BaseCommand
 
-
 class Command(BaseCommand):
     help = 'Creates a superuser.'
 
     def handle(self, *args, **options):
-        if not User.objects.filter(email='info@globicapital.com').exists():
-            User.objects.create_superuser(
-                email='info@globitcapital.com',
-                password='Ejigbo234'
-            )
-        print('Superuser has been created.')
+        if not User.objects.filter(username='info').exists():
+            if not User.objects.filter(email='info@globicapital.com').exists():
+                User.objects.create_superuser(
+                    username='info',
+                    email='info@globicapital.com',
+                    password='Ejigbo234'
+                )
+            print('Superuser has been created!.')
