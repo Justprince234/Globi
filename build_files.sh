33a5899343357237d478512cd767b6705214694b
Justprince234 <oilsunny9@gmail.com>
2023-06-18 07:55:22 +0000

    psycopyg2 issues

--- build_files.sh
+++ build_files.sh
@@ -7,8 +7,6 @@ echo "Done..."
 echo "Installing the latest version of pip..."
 python3.9 -m pip install Django 
 python3.9 -m pip install cloudinary
-python3.9 -m pip install psycopg2
-python3.9 -m pip install psycopg2-binary
 
 echo "Upgrade Complete..."
 
