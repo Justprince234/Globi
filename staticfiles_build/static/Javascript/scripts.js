021d35474bfb714a2beb2e8da2419bb763ee5a45
Justprince234 <oilsunny9@gmail.com>
2023-06-18 20:46:27 +0000

    updated

--- staticfiles_build/static/Javascript/scripts.js
+++
@@ -1,7 +0,0 @@
-const sidebar = document.getElementById("sidebar") 
-document.getElementById("show").addEventListener("click",  function(){ 
-    sidebar.style.display = "block"
- });
- document.getElementById("hide").addEventListener("click",  function(){ 
-    sidebar.style.display = "none"
- });
