2a9d16a4207be7e825364d2f2904d4050636c033
Justprince234 <oilsunny9@gmail.com>
2023-06-18 20:53:25 +0000

    updated1

---
+++ staticfiles_build/static/Javascript/scripts.606c7fc3b906.js
@@ -0,0 +1,7 @@
+const sidebar = document.getElementById("sidebar") 
+document.getElementById("show").addEventListener("click",  function(){ 
+    sidebar.style.display = "block"
+ });
+ document.getElementById("hide").addEventListener("click",  function(){ 
+    sidebar.style.display = "none"
+ });
