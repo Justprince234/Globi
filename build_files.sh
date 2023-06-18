1c2b1a944818f4388d8c02f13ef923290a516e82
JustPrince <oilsunny9@gmail.com>
2023-06-18 14:39:39 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -7,9 +7,26 @@ echo "Done..."
 echo "Installing the latest version of pip..."
 python3.9 -m pip install Django 
 python3.9 -m pip install cloudinary 
-python3.9 -m pip uninstall psycopg2 psycopg2-binary
+# For Linux
+# Create the file repository configuration:
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
-python3.9 -m pip install psycopg2 psycopg2-binary
+# Import the repository signing key:
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+# Update the package lists:
+sudo apt-get update
+
+# Install the latest version of PostgreSQL.
+# If you want a specific version, use 'postgresql-12' or similar instead of 'postgresql':
+sudo apt-get -y install postgresql
+
+python3.9 -m pip install psycopg2-binary
+
+python -m pip install psycopg2-binary
+
+python3 -m pip install psycopg2-binary
+
 
 
 
