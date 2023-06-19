52ff56d32947377b17f9eb9e0ef77607a60680fb
Justprince234 <oilsunny9@gmail.com>
2023-06-19 13:42:27 +0000

    lastest config.

--- build_files.sh
+++ build_files.sh
@@ -11,7 +11,26 @@ echo "Upgrade Complete..."
 
 # Build the project
 echo "Building the project..."
-python -m pip install -r requirements.txt
+python -m pip install asgiref
+python -m pip install certifi
+python -m pip install charset-normalizer
+python -m pip install cloudinary
+python -m pip install Django
+python -m pip install django-cloudinary-storage
+python -m pip install django-smtp-ssl
+python -m pip install django-sslserver
+python -m pip install gunicorn
+python -m pip install idna
+python -m pip install Pillow
+python -m pip install psycopg2-binary
+python -m pip install python-dotenv
+python -m pip install requests
+python -m pip install six
+python -m pip install sqlparse
+python -m pip install urllib3
+python -m pip install whitenoise
+
+
 echo "Build Complete..."
 
 # Collect static files
