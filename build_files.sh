7f8d2188686672a7142f471625ea07e138d62feb
Justprince234 <oilsunny9@gmail.com>
2023-06-19 13:52:23 +0000

    modified bash

--- build_files.sh
+++ build_files.sh
@@ -6,31 +6,14 @@ echo "Done..."
 
 echo "Installing the latest version of pip..."
 python -m pip install --upgrade pip
+sudo apt-get install pkg-config 
+sudo apt-get install libcairo2-dev
 
 echo "Upgrade Complete..."
 
 # Build the project
 echo "Building the project..."
-python -m pip install asgiref
-python -m pip install certifi
-python -m pip install charset-normalizer
-python -m pip install cloudinary
-python -m pip install Django
-python -m pip install django-cloudinary-storage
-python -m pip install django-smtp-ssl
-python -m pip install django-sslserver
-python -m pip install gunicorn
-python -m pip install idna
-python -m pip install Pillow
-python -m pip install psycopg2-binary
-python -m pip install python-dotenv
-python -m pip install requests
-python -m pip install six
-python -m pip install sqlparse
-python -m pip install urllib3
-python -m pip install whitenoise
-
-
+python -m pip install -r requirements.txt
 echo "Build Complete..."
 
 # Collect static files
